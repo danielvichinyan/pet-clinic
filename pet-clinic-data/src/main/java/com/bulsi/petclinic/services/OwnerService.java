de5cc05a398330810503779b967e79c6989a0fc3
danielvichinyan <vichinyan.daniel@gmail.com>
2022-01-26 16:46:26 +0200

    Refactoring the services into a common CRUD service which uses generic types.

--- pet-clinic-data/src/main/java/com/bulsi/petclinic/services/OwnerService.java
+++ pet-clinic-data/src/main/java/com/bulsi/petclinic/services/OwnerService.java
@@ -2,15 +2,7 @@ package com.bulsi.petclinic.services;
 
 import com.bulsi.petclinic.model.Owner;
 
-import java.util.Set;
-
-public interface OwnerService {
+public interface OwnerService extends CrudService<Owner, Long> {
 
     Owner findByLastName(String lastName);
-
-    Owner findById(Long id);
-
-    Owner save(Owner owner);
-
-    Set<Owner> findAll();
 }
--- pet-clinic-data/src/main/java/com/bulsi/petclinic/services/PetService.java
+++ pet-clinic-data/src/main/java/com/bulsi/petclinic/services/PetService.java
@@ -2,13 +2,6 @@ package com.bulsi.petclinic.services;
 
 import com.bulsi.petclinic.model.Pet;
 
-import java.util.Set;
+public interface PetService extends CrudService<Pet, Long> {
 
-public interface PetService {
-
-    Pet findById(Long id);
-
-    Pet save(Pet pet);
-
-    Set<Pet> findAll();
 }
